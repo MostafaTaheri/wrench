ceb0adf15ab951024c2ed613e5904e3ef075027b
Mostafa Taheri <m.taheri@hamrah-mechanic.com>
2024-07-01 08:49:58 +0330

    Add a service extension

--- src/DistributedCache/Extensions/ServiceCollectionExtensions.cs
+++ src/DistributedCache/Extensions/ServiceCollectionExtensions.cs
@@ -1,2 +1,20 @@
-namespace Wrench.DistributedCache;
+using Microsoft.Extensions.DependencyInjection;
+using Wrench.Services.DistributedCache;
+using Wrench.Interfaces.DistributedCache;
 
+namespace Wrench.Extensions.DistributedCache;
+
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddWrenchDistributedRedisCache(this IServiceCollection serviceCollection, string connectionString)
+    {
+        serviceCollection.AddScoped<IWrenchDistributedCache, WrenchDistributedWrapper>();
+        serviceCollection.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = connectionString;
+        });
+
+        return serviceCollection;
+    }
+}
